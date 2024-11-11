dc64da1e3372186cd99bf100d7dbba0886a1a5ee
karina <pakholkova-karina@mail.ru>
2024-11-11 19:16:21 +0500

    Написала устройство клавиатуры для выбора понравившейся новости

---
+++ OOP2024-1/src/main/java/project/Keyboard.java
@@ -0,0 +1,39 @@
+package project;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Keyboard {
+    private static final Map<String, String> buttonTextToCommandMap = new HashMap<>();
+
+    static {
+        buttonTextToCommandMap.put("like №1", "like №1");
+        buttonTextToCommandMap.put("like №2", "like №2");
+        buttonTextToCommandMap.put("like №3", "like №3");
+        buttonTextToCommandMap.put("like №4", "like №4");
+        buttonTextToCommandMap.put("like №5", "like №5");
+    }
+
+    public static ReplyKeyboardMarkup getNewsKeyboard(List<String> newsList) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(true);
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+        for (int i = 0; i < newsList.size(); i++) {
+            KeyboardRow row = new KeyboardRow();
+            String buttonText = "like №" + (i + 1);
+            row.add(new KeyboardButton(buttonText));
+            keyboardRows.add(row);
+        }
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+        return keyboardMarkup;
+    }
+}
